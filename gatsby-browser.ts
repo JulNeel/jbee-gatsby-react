d02f56d930813f466f6a40f5b211fdabd1e03f79
Julien Bruneel <julien.bruneel@gmail.com>
2025-09-09 21:01:23 +0200

    gatsby browser in typescript

---
+++ gatsby-browser.ts
@@ -0,0 +1,37 @@
+import "prismjs";
+import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-typescript";
+import "./src/styles/layers.css.ts";
+import "@wordpress/block-library/build-style/style.css";
+import "@wordpress/block-library/build-style/theme.css";
+import "./src/styles/global-vanilla.css.ts";
+import { copyButton } from "./src/styles/prism-custom.css.ts";
+import type { GatsbyBrowser } from "gatsby";
+
+export const onInitialClientRender: GatsbyBrowser["onInitialClientRender"] = () => {
+  const blocks = document.querySelectorAll("pre code");
+
+  blocks.forEach((block) => {
+    const parent = block.parentElement as HTMLElement | null;
+    if (!parent) return;
+
+    if (parent.querySelector("button")) return;
+
+    const button: HTMLButtonElement = document.createElement("button");
+    button.innerText = "Copier";
+    button.className = copyButton;
+
+    button.addEventListener("click", async () => {
+      try {
+        await navigator.clipboard.writeText(block.textContent ?? "");
+        button.innerText = "Copié !";
+        setTimeout(() => (button.innerText = "Copier"), 2000);
+      } catch (err) {
+        console.error("Erreur lors de la copie :", err);
+      }
+    });
+
+    parent.style.position = "relative";
+    parent.appendChild(button);
+  });
+};
