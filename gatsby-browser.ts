37f6b4afcff84a9a6f58cba52c28d24687b73441
Julien Bruneel <julien.bruneel@gmail.com>
2025-09-30 15:52:42 +0200

    use pris instead of rehype

--- gatsby-browser.ts
+++ gatsby-browser.ts
@@ -1,6 +1,14 @@
 import "prismjs";
 import "prismjs/themes/prism-okaidia.css";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-jsx";
 import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-tsx";
+
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-markup";
 import "@wordpress/block-library/build-style/style.css";
 import "@wordpress/block-library/build-style/theme.css";
 import "./src/styles/global-vanilla.css.ts";
